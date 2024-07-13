f2d233bdd296a32fe5647d1f15b7cc9aa6dce13d
Vijaysolo06 <145049428+Vijaysolo06@users.noreply.github.com>
2024-07-13 12:39:04 -0300

    some button changes

---
+++ script.js
@@ -0,0 +1,6 @@
+let butt1 = document.querySelector(".butt1");
+let inp1 = document.querySelector(".inp1");
+function wheat() {
+    console.log(inp1.value);
+}
+butt1.addEventListener("click",wheat);
